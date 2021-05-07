3c1193bd648719150861e658c7796a5660169b09
sburge93 <stuartburge93@gmail.com>
2021-05-07 16:15:28 +0100

    content updated

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -9,20 +9,20 @@ constructor() {
     let pagesMap = new Map<string, PageModel>();
   pagesMap.set("home", {
     title: "Home",
-    subtitle: "Welcome Home!",
-    content: "Some home content.",
+    subtitle: "Welcome to my final year development project!",
+    content: "Click on about to find out more about this project or go to straight to the data page to see the classified data.",
     image: "./assets/bg03.jpg",
   });
   pagesMap.set("about", {
     title: "About",
-    subtitle: "About Us",
-    content: "Some content about us.",
+    subtitle: "About This Project..",
+    content: "This development project focuses on both machine learning and programming with the aim to create a hatespeech classifier that identifies hateful tweets with real time data streamed from twitter.",
     image: "./assets/bg02.jpg",
   });
   pagesMap.set("contact", {
     title: "Contact",
-    subtitle: "Contact Us",
-    content: "How to contact us.",
+    subtitle: "Feel Free to Contact Me About This Project!",
+    content: "ST20008817@outlook.cardiffmet.ac.uk",
     image: "./assets/bg01.jpg",
   });
   this.pages = pagesMap
