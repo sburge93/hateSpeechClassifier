269b3c6cb351f1b749957c5efb32319ec3341064
sburge93 <stuartburge93@gmail.com>
2021-05-09 14:10:20 +0100

    commit

--- backend/src/test.py
+++ backend/src/test.py
@@ -1,7 +1,6 @@
 import pickle
 
 classifier = pickle.load(open('./backend/random_forest_model.sav', 'rb'))
-result = classifier.predict(['blah'])
-print(result)
+result = classifier.predict(['momma said no pussy cats inside my doghouse'])
 print('the result is')
-print(result[0])
+print(result)
