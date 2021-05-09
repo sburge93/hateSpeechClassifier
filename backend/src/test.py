64e48f1e0b7f8c629d78958179496f4afb1de054
sburge93 <stuartburge93@gmail.com>
2021-05-09 01:28:16 +0100

    updated classifer

--- backend/src/test.py
+++ backend/src/test.py
@@ -1,5 +1,7 @@
 import pickle
 
-classifier = pickle.load(open('./backend/src/randomForest_model.sav', 'rb'))
-result = classifier.predict("some hatespeech")
+classifier = pickle.load(open('./backend/random_forest_model.sav', 'rb'))
+result = classifier.predict(['blah'])
 print(result)
+print('the result is')
+print(result[0])
