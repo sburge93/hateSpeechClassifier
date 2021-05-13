fbe37f554f031f1496c358535412b221f7c5e865
sburge93 <stuartburge93@gmail.com>
2021-05-13 22:56:21 +0100

    graph implemented

--- src/app/page/graph.component.ts
+++ src/app/page/graph.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import {HateSpeechApiService} from '../api.service/HateSpeechApiService';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HateSpeechApiService } from '../api.service/HateSpeechApiService';
 import { MatCardModule } from '@angular/material/card';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import { TweetModel } from 'src/models/TweetModel';
 import { Label, Color } from 'ng2-charts';
+import { ChartDataSets } from 'chart.js';
 
 
 @Component({
@@ -14,26 +15,13 @@ import { Label, Color } from 'ng2-charts';
 export class GraphComponent implements OnInit, OnDestroy {
   hateSpeechModelSubs: Subscription;
   hateSpeechModel: TweetModel[];
-  // public chartLabels: Label [] = [];
-  // public chartData: number [] = [];
+  public chartLabels: Label[] = [];
+  public chartData: ChartDataSets[] = [];
   public loaded: Boolean;
 
   constructor(private hatespeechApi: HateSpeechApiService) {
   }
-  chartData = [
-    {
-      data: [330, 600, 260, 700],
-      label: 'Account A'
-    }
-  
-  ];
 
-  chartLabels = [
-    'January',
-    'February',
-    'March',
-    'April'
-  ];
 
   lineChartColors: Color[] = [
     {
@@ -45,24 +33,27 @@ export class GraphComponent implements OnInit, OnDestroy {
     responsive: true
   };
 
-ngOnInit() {
-    this.hateSpeechModelSubs = this.hatespeechApi
-      .getHateSpeech()
-      .subscribe(res => {
-        console.log(res)
-          this.hateSpeechModel = res;
-          // this.chartLabels = this.hateSpeechModel.map (item => item.minute);
-          // this.chartData = this.hateSpeechModel.map(item => item.count);
-          console.log(this.hateSpeechModel)
-          // console.log(this.chartData)
-          // console.log(this.chartLabels)
-          this.loaded = true
-        },
-        console.error
-      );
+  ngOnInit() {
+    setTimeout(() => {
+      this.getData();
+    }, 5000);
   }
 
+  getData() {
+  this.hateSpeechModelSubs = this.hatespeechApi
+    .getHateSpeech()
+    .subscribe(res => {
+      console.log(res)
+      this.hateSpeechModel = res;
+      this.chartLabels = this.hateSpeechModel.map(item => item.minute);
+      this.chartData = [{ data: this.hateSpeechModel.map(item => item.count), label: 'Tweets' }];
+      console.log(this.hateSpeechModel)
+      this.loaded = true
+    },
+      console.error);
+}
+
 ngOnDestroy() {
-    this.hateSpeechModelSubs.unsubscribe();
-  }
+  this.hateSpeechModelSubs.unsubscribe();
+}
 }
